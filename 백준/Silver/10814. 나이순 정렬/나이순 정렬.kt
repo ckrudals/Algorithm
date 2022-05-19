81f1a980cd5f5000cfc441f7b304a46a3cd7c58a
차경민 <68880067+ckrudals@users.noreply.github.com>
2022-05-19 14:18:39 +0900

    [Silver V] Title: 나이순 정렬, Time: 868 ms, Memory: 81380 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.kt
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.kt
@@ -1,14 +1,23 @@
 fun main() {
-    val num = readLine()!!.toInt()
-    val list = mutableListOf<Pair<Int,String>>()
-    for(i in 0 until num){
-        val line = readLine()!!.split(" ")
-        list.add(Pair(line[0].toInt(),line[1]))
+    val br = System.`in`.bufferedReader()
+    val bw = System.out.bufferedWriter()
+    val input = br.readLine().toInt()
+    val list = ArrayList<Pair<Int, String>>()
+
+    repeat(input) {
+        val ages = br.readLine().split(" ").map { it }
+        list.add(Pair(ages[0].toInt(), ages[1]))
     }
-    list.sortWith{a,b -> a.first - b.first}
+    list.sortWith { d1, d2 ->
+
+        d1.first - d2.first
+    }
+
+    for (i in 0 until input) {
+        bw.write("${list[i].first} ${list[i].second}\n")
+    }
+
+    bw.flush()
 
 
-    for(i in 0 until num){
-        println("${list[i].first} ${list[i].second}")
-    }
 }
